ceb5cb0c9333db9777cce841f0531d34f28036f1
ccckmit <ccckmit@gmail.com>
2017-12-19 14:41:28 +0800

    add gulpfile to translate markdown extension .mde into .md

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,8 @@ gulp.task('mde2md', function(){
   return gulp.src('**/*.mde')
     .pipe(rename(function (path) {
        path.extname = ".md";
-       console.log('path=', path)
     }))
     .pipe(replace(/\$(.*?)\$/g, function(match, p1, offset, string) {
-        console.log('match=', match, ' p1=', p1)
         return `<img src="https://latex.codecogs.com/gif.latex?${encodeURIComponent(p1)}"/>`
     }))
     .pipe(gulp.dest(''))
