c657027f329c2d17a0ad1ca29a1d8825b33ba084
ccckmit <ccckmit@gmail.com>
2017-12-21 20:46:16 +0800

    add FFT, huffmanCode, SAT, viterbi, ... algorithm

--- course/algorithm/number/gcd.js
+++ course/algorithm/number/gcd.js
@@ -2,10 +2,8 @@
 // https://stackoverflow.com/questions/32042240/euclids-algorithm-javascript
 
 var gcd = function(a, b) {  
-    if (!b) {  
-        return a;  
-    }
-    return gcd(b, a % b);  
- }
+  if (!b) return a
+  return gcd(b, a % b)
+}
 
- console.log(gcd(462, 910));
+console.log(gcd(462, 910))
